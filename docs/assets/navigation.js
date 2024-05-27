e239109b3341f0faba4eaf06a8544879bd6cb59f
Kestutis <kestutis.girnius@gmail.com>
2024-05-27 14:30:07 +0300

    docs

---
+++ docs/assets/navigation.js
@@ -0,0 +1,1 @@
+window.navigationData = "data:application/octet-stream;base64,H4sIAAAAAAAACouuVipJrShRslJyTswrSyx2zs8tyM9LzStR0lEqSCzJULJSSs5JLC5OLdZHU6CXUZKbo6SjlJ2Zl6JkZWhkURsLAPMRYnpNAAAA"
--- src/lib/ng-image-canvas.component.ts
+++ src/lib/ng-image-canvas.component.ts
@@ -1,23 +1,34 @@
-
-import { Component, ElementRef, EventEmitter, HostListener, Input, OnChanges, OnInit, Output, SimpleChanges, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  HostListener,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+  ViewChild,
+} from '@angular/core';
 import { DraggableImage } from './utils/draggable-image';
 
 @Component({
   selector: 'ng-image-canvas',
   templateUrl: './ng-image-canvas.component.html',
-  styleUrls: ['./styles.css']
+  styleUrls: ['./styles.css'],
 })
 export class CanvasComponent implements OnInit, OnChanges {
   @Input() width: number = 800;
   @Input() height: number = 600;
   @Input() editing: boolean = true;
   @Input() resizeHandleSize: number = 10;
-  @Input() resizeHandleColor: string = "gray";
+  @Input() resizeHandleColor: string = 'gray';
   @Input() closeButtonSize: number = 20;
-  @Input() closeButtonBackgroundColor: string = "red";
-  @Input() closeButtonFontColor: string = "white";
+  @Input() closeButtonBackgroundColor: string = 'red';
+  @Input() closeButtonFontColor: string = 'white';
   @Output() imagesChange = new EventEmitter<DraggableImage[]>();
-  @ViewChild('canvas', { static: true }) canvasRef!: ElementRef<HTMLCanvasElement>;
+  @ViewChild('canvas', { static: true })
+  canvasRef!: ElementRef<HTMLCanvasElement>;
 
   private ctx!: CanvasRenderingContext2D;
   private images: DraggableImage[] = [];
@@ -27,6 +38,12 @@ export class CanvasComponent implements OnInit, OnChanges {
   private isDragging: boolean = false;
   private isShiftPressed: boolean = false;
 
+  /**
+   * Initializes the canvas component.
+   * Sets the width and height of the canvas, gets the 2D rendering context, and draws initial images.
+   *
+   * @returns void
+   */
   ngOnInit(): void {
     const canvas = this.canvasRef.nativeElement;
     canvas.width = this.width;
@@ -35,6 +52,13 @@ export class CanvasComponent implements OnInit, OnChanges {
     this.drawImages();
   }
 
+  /**
+   * Responds to changes in the input properties.
+   * Specifically handles changes to the `editing` property, resetting interactions and redrawing images.
+   *
+   * @param changes - An object of type `SimpleChanges` that holds the current and previous values of the input properties.
+   * @returns void
+   */
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['editing']) {
       this.resetInteractions();
@@ -42,19 +66,32 @@ export class CanvasComponent implements OnInit, OnChanges {
     }
   }
 
+  /**
+   * Resets interaction states of the images.
+   * Clears the selected image index and dragging state, and stops resizing for all images.
+   *
+   * @returns void
+   */
   resetInteractions(): void {
     this.selectedImageIndex = null;
     this.isDragging = false;
-    this.images.forEach(image => {
+    this.images.forEach((image) => {
       image.isResizing = false;
     });
   }
 
+  /**
+   * Handles the drop event for adding new images to the canvas.
+   * Reads the dropped file, creates a new `DraggableImage` object, adds it to the images array, and redraws the images.
+   *
+   * @param event - The drop event containing the file to be added.
+   * @returns void
+   */
   onDrop(event: DragEvent): void {
     event.preventDefault();
     event.stopPropagation();
-    if(!this.editing || event == null || event.dataTransfer == null){
-      return
+    if (!this.editing || event == null || event.dataTransfer == null) {
+      return;
     }
     const file = event.dataTransfer.files[0];
     const reader = new FileReader();
@@ -67,7 +104,10 @@ export class CanvasComponent implements OnInit, OnChanges {
           x: 0,
           y: 0,
           width: img.width <= this.width ? img.width : 100,
-          height: img.height <= this.height ? img.height : img.height / img.width * 100,
+          height:
+            img.height <= this.height
+              ? img.height
+              : (img.height / img.width) * 100,
         };
         this.images.push(draggableImage);
         this.drawImages();
@@ -77,13 +117,25 @@ export class CanvasComponent implements OnInit, OnChanges {
     reader.readAsDataURL(file);
   }
 
+  /**
+   * Draws all images on the canvas.
+   * Clears the canvas, then draws each image and optionally their resize handles and close buttons.
+   *
+   * @returns void
+   */
   drawImages(): void {
-    if(this.ctx == null){
+    if (this.ctx == null) {
       return;
     }
     this.ctx.clearRect(0, 0, this.width, this.height);
     this.images.forEach((draggableImage) => {
-      this.ctx!.drawImage(draggableImage.img, draggableImage.x, draggableImage.y, draggableImage.width, draggableImage.height);
+      this.ctx!.drawImage(
+        draggableImage.img,
+        draggableImage.x,
+        draggableImage.y,
+        draggableImage.width,
+        draggableImage.height
+      );
       if (this.editing) {
         this.drawResizeHandles(draggableImage);
         this.drawCloseButton(draggableImage);
@@ -91,22 +143,54 @@ export class CanvasComponent implements OnInit, OnChanges {
     });
   }
 
+  /**
+   * Draws resize handles on the given image.
+   *
+   * @param image - The `DraggableImage` object on which to draw resize handles.
+   * @returns void
+   */
   drawResizeHandles(image: DraggableImage): void {
     this.ctx!.fillStyle = this.resizeHandleColor;
-    this.ctx!.fillRect(image.x + image.width - this.resizeHandleSize, image.y + image.height - this.resizeHandleSize, this.resizeHandleSize, this.resizeHandleSize);
+    this.ctx!.fillRect(
+      image.x + image.width - this.resizeHandleSize,
+      image.y + image.height - this.resizeHandleSize,
+      this.resizeHandleSize,
+      this.resizeHandleSize
+    );
   }
 
+  /**
+   * Draws a close button on the given image.
+   *
+   * @param image - The `DraggableImage` object on which to draw a close button.
+   * @returns void
+   */
   drawCloseButton(image: DraggableImage): void {
     this.ctx.fillStyle = this.closeButtonBackgroundColor;
-    this.ctx.fillRect(image.x + image.width - this.closeButtonSize, image.y, this.closeButtonSize, this.closeButtonSize);
+    this.ctx.fillRect(
+      image.x + image.width - this.closeButtonSize,
+      image.y,
+      this.closeButtonSize,
+      this.closeButtonSize
+    );
     this.ctx.fillStyle = this.closeButtonFontColor;
     this.ctx.font = '16px Arial';
-    this.ctx.fillText('x', image.x + image.width - this.closeButtonSize / 2 - 4, image.y + this.closeButtonSize / 2 + 5);
+    this.ctx.fillText(
+      'x',
+      image.x + image.width - this.closeButtonSize / 2 - 4,
+      image.y + this.closeButtonSize / 2 + 5
+    );
   }
 
+  /**
+   * Handles the mouse down event on the canvas.
+   * Checks if the mouse down is on an image or a close button, sets the selected image index, and prepares for dragging or resizing.
+   *
+   * @param event - The mouse event.
+   * @returns void
+   */
   onMouseDown(event: MouseEvent): void {
     if (!this.editing) return; // Disable mouse down when editing is disabled
-
     const { offsetX, offsetY } = event;
 
     // Check if click is on the close button
@@ -115,10 +199,14 @@ export class CanvasComponent implements OnInit, OnChanges {
         x: img.x + img.width - this.closeButtonSize,
         y: img.y,
         width: this.closeButtonSize,
-        height: this.closeButtonSize
+        height: this.closeButtonSize,
       };
-      return offsetX >= closeButtonBounds.x && offsetX <= closeButtonBounds.x + closeButtonBounds.width &&
-             offsetY >= closeButtonBounds.y && offsetY <= closeButtonBounds.y + closeButtonBounds.height;
+      return (
+        offsetX >= closeButtonBounds.x &&
+        offsetX <= closeButtonBounds.x + closeButtonBounds.width &&
+        offsetY >= closeButtonBounds.y &&
+        offsetY <= closeButtonBounds.y + closeButtonBounds.height
+      );
     });
 
     if (this.selectedImageIndex !== -1) {
@@ -131,12 +219,16 @@ export class CanvasComponent implements OnInit, OnChanges {
 
     // Check if click is on the image or resize handle
     this.selectedImageIndex = this.images.findIndex((img) => {
-      const inImageBounds = offsetX >= img.x && offsetX <= img.x + img.width &&
-                            offsetY >= img.y && offsetY <= img.y + img.height;
-      const inResizeBounds = offsetX >= img.x + img.width - this.resizeHandleSize &&
-                             offsetX <= img.x + img.width &&
-                             offsetY >= img.y + img.height - this.resizeHandleSize &&
-                             offsetY <= img.y + img.height;
+      const inImageBounds =
+        offsetX >= img.x &&
+        offsetX <= img.x + img.width &&
+        offsetY >= img.y &&
+        offsetY <= img.y + img.height;
+      const inResizeBounds =
+        offsetX >= img.x + img.width - this.resizeHandleSize &&
+        offsetX <= img.x + img.width &&
+        offsetY >= img.y + img.height - this.resizeHandleSize &&
+        offsetY <= img.y + img.height;
       if (inResizeBounds) {
         img.isResizing = true;
         img.initialWidth = img.width;
@@ -153,6 +245,13 @@ export class CanvasComponent implements OnInit, OnChanges {
     }
   }
 
+  /**
+   * Handles the mouse move event on the canvas.
+   * Drags or resizes the selected image based on the mouse movement.
+   *
+   * @param event - The mouse event.
+   * @returns void
+   */
   onMouseMove(event: MouseEvent): void {
     if (this.selectedImageIndex !== null) {
       const { offsetX, offsetY } = event;
@@ -161,22 +260,46 @@ export class CanvasComponent implements OnInit, OnChanges {
       if (img.isResizing) {
         const aspectRatio = img.initialWidth! / img.initialHeight!;
         if (this.isShiftPressed) {
-          const newWidth = Math.max(10, (img.initialWidth || 0) + (offsetX - img.x - (img.initialWidth || 0)));
+          const newWidth = Math.max(
+            10,
+            (img.initialWidth || 0) +
+              (offsetX - img.x - (img.initialWidth || 0))
+          );
           const newHeight = newWidth / aspectRatio;
           img.width = newWidth;
           img.height = newHeight;
         } else {
-          img.width = Math.max(10, (img.initialWidth || 0) + (offsetX - img.x - (img.initialWidth || 0)));
-          img.height = Math.max(10, (img.initialHeight || 0) + (offsetY - img.y - (img.initialHeight || 0)));
+          img.width = Math.max(
+            10,
+            (img.initialWidth || 0) +
+              (offsetX - img.x - (img.initialWidth || 0))
+          );
+          img.height = Math.max(
+            10,
+            (img.initialHeight || 0) +
+              (offsetY - img.y - (img.initialHeight || 0))
+          );
         }
       } else if (this.isDragging) {
-        img.x = Math.min(Math.max(0, offsetX - this.offsetX), this.width - img.width);
-        img.y = Math.min(Math.max(0, offsetY - this.offsetY), this.height - img.height);
+        img.x = Math.min(
+          Math.max(0, offsetX - this.offsetX),
+          this.width - img.width
+        );
+        img.y = Math.min(
+          Math.max(0, offsetY - this.offsetY),
+          this.height - img.height
+        );
       }
       this.drawImages();
     }
   }
 
+  /**
+   * Handles the mouse up event on the canvas.
+   * Stops dragging or resizing the selected image.
+   *
+   * @returns void
+   */
   onMouseUp(): void {
     if (this.selectedImageIndex !== null) {
       const img = this.images[this.selectedImageIndex];
@@ -186,6 +309,13 @@ export class CanvasComponent implements OnInit, OnChanges {
     this.isDragging = false;
   }
 
+  /**
+   * Handles the key down event on the document.
+   * Sets the `isShiftPressed` flag when the Shift key is pressed.
+   *
+   * @param event - The keyboard event.
+   * @returns void
+   */
   @HostListener('document:keydown', ['$event'])
   onKeyDown(event: KeyboardEvent): void {
     if (event.key === 'Shift') {
@@ -193,6 +323,13 @@ export class CanvasComponent implements OnInit, OnChanges {
     }
   }
 
+  /**
+   * Handles the key up event on the document.
+   * Clears the `isShiftPressed` flag when the Shift key is released.
+   *
+   * @param event - The keyboard event.
+   * @returns void
+   */
   @HostListener('document:keyup', ['$event'])
   onKeyUp(event: KeyboardEvent): void {
     if (event.key === 'Shift') {
@@ -200,21 +337,43 @@ export class CanvasComponent implements OnInit, OnChanges {
     }
   }
 
+  /**
+   * Handles the drag over event on the canvas.
+   * Prevents the default behavior to allow dropping.
+   *
+   * @param event - The drag event.
+   * @returns void
+   */
   @HostListener('dragover', ['$event'])
   onDragOver(event: DragEvent): void {
     event.preventDefault();
   }
 
+  /**
+   * Emits the current list of draggable images.
+   *
+   * @returns void
+   */
   private emitImagesChange(): void {
     this.imagesChange.emit(this.images);
   }
 
-  // Method to get the list of images
+  /**
+   * Gets the list of images on the canvas.
+   *
+   * @returns An array of `DraggableImage` objects.
+   */
   getImages(): DraggableImage[] {
     return this.images;
   }
 
-  // Method to set the list of images
+  /**
+   * Sets the list of images on the canvas.
+   * Redraws the images after setting.
+   *
+   * @param images - An array of `DraggableImage` objects.
+   * @returns void
+   */
   setImages(images: DraggableImage[]): void {
     this.images = images;
     this.drawImages();
